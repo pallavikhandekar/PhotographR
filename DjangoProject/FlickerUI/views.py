50590b06a75830acef0678fbce4c747e5b0ef107
Pallavi <pallavikhandekar212@gmail.com>
2013-10-26 00:42:43 -0700

    start

---
+++ DjangoProject/FlickerUI/views.py
@@ -0,0 +1,14 @@
+# Create your views here.
+#from django.http import HttpResponse
+from django.shortcuts import render
+import csv
+import os
+
+def index(request):
+
+    fname = os.path.join(os.path.dirname(__file__), 'flickpics.csv')
+    with open(fname, 'rb') as csvfile:
+        spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
+        rows=[row for row in spamreader]
+  #  return HttpResponse("Hello, world. You're at the poll index.")
+    return  render(request, 'index.html', {"rows": rows})
