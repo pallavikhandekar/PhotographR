5936088e28c610756e72138998f202b0bb103a3f
Pallavi <pallavikhandekar212@gmail.com>
2013-10-26 03:08:29 -0700

    Some Search html changes Submit

--- DjangoProject/FlickerUI/views.py
+++ DjangoProject/FlickerUI/views.py
@@ -7,22 +7,17 @@ import csv
 import os
 
 def index(request):
-    print "CLLED ME"
     if request.method == 'POST':
+        if 'searchAttribute' in request.POST:
+            print request.POST['searchAttribute']
         fname = os.path.join(os.path.dirname(__file__), 'flickpics.csv')
         with open(fname, 'rb') as csvfile:
             spamreader = csv.reader(csvfile, delimiter=' ', quotechar='|')
             rows=[row for row in spamreader]
       #  return HttpResponse("Hello, world. You're at the poll index.")
-        return  render_to_response(request, 'index.html', {"rows": rows})
+        return render(request, 'index.html', {"rows": rows})
 
 
 def search(request):
     return render(request,'SearchImage.html')
 
-def search_form(request):
-    if 'searchAttribute' in request.GET:
-        message = 'You searched for: %r' % request.GET['searchAttribute']
-    else:
-        message = 'You submitted an empty form.'
-    return HttpResponse(message)
